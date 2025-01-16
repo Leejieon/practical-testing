0d5c6017dc154be3271461c89fd1ee759c25d998
leejieon <dlwldjs7544@naver.com>
2025-01-16 22:06:56 +0900

    test: test for product

---
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductTypeTest.java
@@ -0,0 +1,34 @@
+package sample.cafekiosk.spring.domain.product;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ProductTypeTest {
+
+	@DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+	@Test
+	void containsStockType() {
+		// Given
+		ProductType givenType = ProductType.HANDMADE;
+
+		// When
+		boolean result = ProductType.containsStockType(givenType);
+
+		// Then
+		assertThat(result).isFalse();
+	}
+
+	@DisplayName("상품 타입이 재고 관련 타입인지를 체크한다.")
+	@Test
+	void containsStockType2() {
+		// Given
+		ProductType givenType = ProductType.BAKERY;
+
+		// When
+		boolean result = ProductType.containsStockType(givenType);
+
+		// Then
+		assertThat(result).isTrue();
+	}
+}
